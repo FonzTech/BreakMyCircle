aecb70c64166efd0d2de48bdc1500b8ba8734902
Alfonso <9438351+FonzTech@users.noreply.github.com>
2021-08-10 09:39:14 +0200

    Move common defines to CommonTypes

--- src/Common/CommonTypes.h
+++ src/Common/CommonTypes.h
@@ -1,5 +1,45 @@
 #pragma once
 
+#define RESOURCE_MESH_PLANE_SPRITE "mesh_plane_sprite"
+#define RESOURCE_MESH_PLANE_WATER "mesh_plane_water"
+#define RESOURCE_MESH_SAND_FLOOR "mesh_plane_sand_floor"
+#define RESOURCE_MESH_PLANE_FLAT "mesh_plane_flat"
+#define RESOURCE_MESH_CUBE "mesh_plane_cube"
+
+#define RESOURCE_TEXTURE_SPARKLES "tex_sparkles"
+#define RESOURCE_TEXTURE_BUBBLE_RED "tex_bubble_red"
+#define RESOURCE_TEXTURE_BUBBLE_GREEN "tex_bubble_green"
+#define RESOURCE_TEXTURE_BUBBLE_BLUE "tex_bubble_blue"
+#define RESOURCE_TEXTURE_BUBBLE_YELLOW "tex_bubble_yellow"
+#define RESOURCE_TEXTURE_BUBBLE_ORANGE "tex_bubble_orange"
+#define RESOURCE_TEXTURE_BUBBLE_PURPLE "tex_bubble_purple"
+#define RESOURCE_TEXTURE_BUBBLE_CYAN "tex_bubble_cyan"
+#define RESOURCE_TEXTURE_SKYBOX_1_PX "tex_skybox_1_px"
+#define RESOURCE_TEXTURE_WATER_DISPLACEMENT "tex_water_dm"
+#define RESOURCE_TEXTURE_WATER_TEXTURE "tex_water_tm"
+#define RESOURCE_TEXTURE_WORLD_1_WEM "tex_world_1_wem"
+#define RESOURCE_TEXTURE_CUBEMAP_SKYBOX_1 "tex_cubemap_skybox_1"
+#define RESOURCE_TEXTURE_GUI_SETTINGS "tex_gui_settings"
+#define RESOURCE_TEXTURE_GUI_LEVEL_PANEL "tex_gui_level_panel"
+#define RESOURCE_TEXTURE_GUI_STAR_GRAY "tex_gui_star_gray"
+#define RESOURCE_TEXTURE_GUI_BACK_ARROW "tex_gui_back_arrow"
+#define RESOURCE_TEXTURE_WHITE "tex_white"
+
+#define RESOURCE_SHADER_COLORED_PHONG "shader_colored_phong"
+#define RESOURCE_SHADER_COLORED_PHONG_2 "shader_colored_phong_2"
+#define RESOURCE_SHADER_TEXTURED_PHONG_DIFFUSE "shader_textured_phong"
+#define RESOURCE_SHADER_TEXTURED_PHONG_DIFFUSE_2 "shader_textured_phong_2"
+#define RESOURCE_SHADER_SPRITE "shader_sprite"
+#define RESOURCE_SHADER_WATER "shader_water"
+#define RESOURCE_SHADER_CUBEMAP "shader_cubemap"
+#define RESOURCE_SHADER_FLAT3D "shader_flat3d"
+#define RESOURCE_SHADER_DISTANCE_FIELD_VECTOR "shader_distance_field_vector"
+
+#define RESOURCE_FONT_UBUNTU_TITLE "ubuntu-title"
+
+#define RESOURCE_PATH_PREFIX "path_"
+#define RESOURCE_PATH_NEW_SPHERE "new_sphere"
+
 #define GOT_PLAYER 1
 #define GOT_BUBBLE 2
 #define GOT_PROJECTILE 3
