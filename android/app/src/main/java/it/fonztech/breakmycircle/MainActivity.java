35d3dc8cefc020a70543ea6f3aaddc139f4d9d7d
Alfonso <9438351+FonzTech@users.noreply.github.com>
2021-09-03 23:00:12 +0200

    Add stub for Android compile chain

---
+++ android/app/src/main/java/it/fonztech/breakmycircle/MainActivity.java
@@ -0,0 +1,12 @@
+package it.fonztech.breakmycircle;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends NativeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
