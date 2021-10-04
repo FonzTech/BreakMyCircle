ea142ab80c16201962bc9a707a3d9016a19f52f8
Alfonso <alfonso@iMac-di-Alfonso.homenet.telecomitalia.it>
2021-10-04 17:31:37 +0200

    Add static plugin import for iOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include "Engine.h"
 
+#if defined(CORRADE_TARGET_IOS_SIMULATOR) or defined(CORRADE_TARGET_IOS)
+#include <MagnumPlugins/AnyImageImporter/importStaticPlugin.cpp>
+#include <MagnumPlugins/AnySceneImporter/importStaticPlugin.cpp>
+#include <MagnumPlugins/PngImporter/importStaticPlugin.cpp>
+#include <MagnumPlugins/StbTrueTypeFont/importStaticPlugin.cpp>
+#include <MagnumPlugins/StbVorbisAudioImporter/importStaticPlugin.cpp>
+#include <MagnumPlugins/TinyGltfImporter/importStaticPlugin.cpp>
+#endif
+
 using namespace Magnum;
 
 MAGNUM_APPLICATION_MAIN(Engine)
