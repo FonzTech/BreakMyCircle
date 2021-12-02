8c417fa309b35b9dc11c07d759d957fc4015c36e
Alfonso <alfonso.pauciello@etuitus.it>
2021-12-02 17:20:09 +0100

    Improve text size for numbers

--- src/Common/CustomRenderers/LSNumberRenderer.cpp
+++ src/Common/CustomRenderers/LSNumberRenderer.cpp
@@ -11,9 +11,36 @@ LSNumberRenderer::LSNumberRenderer(const Vector2i & size, const std::string & te
 	mOverlayGui->setIdentityCanvasSize();
 
 	// Create overlay text
+	Float ts;
+	switch (Int(text.length()))
+	{
+	case 0: {
+		ts = 1.0f;
+		break;
+	}
+	case 1: {
+		ts = 0.625f;
+		break;
+	}
+	case 2: {
+		ts = 0.525f;
+		break;
+	}
+	case 3: {
+		ts = 0.4f;
+		break;
+	}
+	case 4: {
+		ts = 0.3f;
+		break;
+	}
+	default:
+		ts = 0.75f - 0.125f * Float(text.length());
+	}
+
 	mOverlayText = std::make_shared<OverlayText>(-1, Text::Alignment::MiddleCenter, UnsignedInt(text.length()));
 	mOverlayText->mPosition = Vector3(0.0f);
-	mOverlayText->setSize(Vector2(0.75f - 0.125f * Float(text.length())));
+	mOverlayText->setSize(Vector2(ts));
 	mOverlayText->mColor = Color4(0.0f, 0.0f, 0.0f, 1.0f);
 	mOverlayText->mOutlineColor = Color4(0.0f);
 	mOverlayText->mOutlineRange = Vector2(0.5f, 1.0f);
