d43b10428203bc24e545751e1aacce4d0de635e2
FonzTech <9438351+FonzTech@users.noreply.github.com>
2021-12-23 02:05:10 +0100

    Improve app state callbacks for iOS

--- src/Game/Callbacks/IAppStateCallback.cpp
+++ src/Game/Callbacks/IAppStateCallback.cpp
@@ -8,6 +8,17 @@ IAppStateCallback::IAppStateCallback()
 
 IAppStateCallback::~IAppStateCallback()
 {
+#if defined(CORRADE_TARGET_IOS) || defined(CORRADE_TARGET_IOS_SIMULATOR)
+    /*
+        applicationWillTerminate is not called, on iOS, when
+        the user kills the app through the task manager.
+     */
+    if (RoomManager::singleton == nullptr)
+    {
+        return;
+    }
+#endif
+    
     auto& s = RoomManager::singleton->mAppStateCallbacks;
     const auto& it = s.find(this);
     if (it != s.end())
